41db15ce9afb8e6bd9d641bf7c7243357256c5be
AleksandraV <am.amsti@gmail.com>
2022-04-18 02:06:12 +0300

    some file fixes

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import werkzeug.exceptions
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://sasha:11qa@localhost:5432/auto_api"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
